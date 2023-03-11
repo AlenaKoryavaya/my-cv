0a791a3af81ee95e6899f0b8837bba502e299c5a
Alena Koryavaya <elena.koryavaya@gmail.com>
2023-03-11 14:59:50 +0300

    fix: summary text

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -7,14 +7,18 @@ const Summary = () => {
             <p className="summary__text">
                 For as long as I can remember, I have always liked to keep up with the times. As the
                 internet evolved, more and more useful applications appeared and websites began to
-                look simply amazing. And I liked to consider myself a power user.
-                <br /> <br /> And because I didn’t know anything about Tech, my interest increased
-                every day. That interest also led me to front-end development. I started with layout
-                basics HTML5, CSS3, JavaScript and then moved on to React. I am also currently
-                studying Redux. It's so exciting because every day you learn something new and I
-                really like it. I also want to continue my education further and I think it's time
-                to try myself as an trainee JavaScript developer. I will be strengthing my existing
-                knowledge and gain new ones to become a real developer.
+                look simply amazing. And I liked to consider myself a power user. And because I
+                didn’t know anything about Tech, my interest increased every day. That interest also
+                led me to front-end development and I desided to know more about it. I started with
+                the basics of layout and gradually moved to a programming language. That was an
+                absolute delight for me as my work began to come to life.{" "}
+            </p>
+            <p className="summary__text">
+                This inspiration feeling guided me further to explore useful development tools and
+                libraries. Currently I am learning TypeScript and try to use it. I love the
+                development process, I enjoy writing code, fixing bugs and also learning new tools.
+                I'm not going to stop there, but I want to become a more experienced and competent
+                developer.
             </p>
         </section>
     );
